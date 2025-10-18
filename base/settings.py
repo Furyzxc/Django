9ec06859885df081b7cdc453f9087af43ec52fbb
Furyzxc <serhii.ananiev2@mail.dcu.ie>
2025-10-18 21:54:59 +0100

    Initial Django project setup with shop app

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-btoj#wx$7kxc*ork-fqt()o7l-5-=+l4n301z3#3x5zael87j&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "shop"
 ]
 
 MIDDLEWARE = [
